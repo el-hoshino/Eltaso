f5a76cb3be2cbf888e7479014878e2ce68153610
星野恵瑠 <shonetesmith@gmail.com>
2016-07-05 15:53:37 +0900

    Init Xcode project

---
+++ Eltaso/Eltaso/Eltaso.h
@@ -0,0 +1,19 @@
+//
+//  Eltaso.h
+//  Eltaso
+//
+//  Created by 史翔新 on 2016/07/05.
+//  Copyright © 2016年 Crazism. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Eltaso.
+FOUNDATION_EXPORT double EltasoVersionNumber;
+
+//! Project version string for Eltaso.
+FOUNDATION_EXPORT const unsigned char EltasoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Eltaso/PublicHeader.h>
+
+

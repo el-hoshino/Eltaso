c5458d6eadb7cb7330cbfbfa75cc52a45e629079
星野恵瑠 <shonetesmith@gmail.com>
2016-07-05 16:05:40 +0900

    Bool extension

---
+++ Eltaso/Eltaso/Bool.swift
@@ -0,0 +1,17 @@
+//
+//  Bool.swift
+//  Eltaso
+//
+//  Created by 史翔新 on 2016/07/05.
+//  Copyright © 2016年 Crazism. All rights reserved.
+//
+
+import Foundation
+
+public extension Bool {
+	
+	mutating public func reverse() {
+		self = !self
+	}
+	
+}

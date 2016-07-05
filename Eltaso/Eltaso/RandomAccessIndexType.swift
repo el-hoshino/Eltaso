10cb74b6036bdf454a6ebb1c170bdcbcf8139094
星野恵瑠 <shonetesmith@gmail.com>
2016-07-05 16:06:50 +0900

    RandomAccessIndexType

---
+++ Eltaso/Eltaso/RandomAccessIndexType.swift
@@ -0,0 +1,29 @@
+//
+//  RandomAccessIndexType.swift
+//  Eltaso
+//
+//  Created by 史翔新 on 2016/07/05.
+//  Copyright © 2016年 Crazism. All rights reserved.
+//
+
+import Foundation
+
+public extension RandomAccessIndexType {
+	
+	var increased: Self {
+		return self.advancedBy(1)
+	}
+	
+	var decreased: Self {
+		return self.advancedBy(-1)
+	}
+	
+	mutating func increase(by n: Self.Distance) {
+		self = self.advancedBy(n)
+	}
+	
+	mutating func decrease(by n: Self.Distance) {
+		self = self.advancedBy(-n)
+	}
+	
+}

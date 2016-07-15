efe4205664a2c0be0e31dc585b5e10bd9d5f5ad0
史 翔新 <shi_xiangxin@mages.co.jp>
2016-07-15 22:16:31 +0900

    Int extension

---
+++ Eltaso/Eltaso/Int.swift
@@ -0,0 +1,19 @@
+//
+//  Int.swift
+//  Eltaso
+//
+//  Created by 史　翔新 on 2016/07/15.
+//  Copyright © 2016年 Crazism. All rights reserved.
+//
+
+import Foundation
+
+public extension Int {
+	
+	static func createRandom(range range: Range<Int>) -> Int {
+		let rangeLength = range.endIndex - range.startIndex
+		let random = arc4random_uniform(UInt32(rangeLength))
+		return Int(random) + range.startIndex
+	}
+	
+}

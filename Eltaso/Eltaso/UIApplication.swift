3569ad0d0992dcf541b6250cdf8b5d0458a2d55a
史 翔新 <shi_xiangxin@mages.co.jp>
2016-08-19 18:18:16 +0900

    UIApplication extension

---
+++ Eltaso/Eltaso/UIApplication.swift
@@ -0,0 +1,25 @@
+//
+//  UIApplication.swift
+//  Eltaso
+//
+//  Created by 史　翔新 on 2016/08/19.
+//  Copyright © 2016年 Crazism. All rights reserved.
+//
+
+import UIKit
+
+public extension UIApplication {
+	
+	public var currentViewController: UIViewController? {
+		
+		guard var controller = self.keyWindow?.rootViewController else {
+			return nil
+		}
+		while let presentedController = controller.presentedViewController {
+			controller = presentedController
+		}
+		return controller
+		
+	}
+	
+}

d8dca2e1278530548a261447186f52a5ae8274b3
史 翔新 <shi_xiangxin@mages.co.jp>
2016-09-09 21:53:38 +0900

    CustomOperators update which added CoutableClosedRange …= operation

--- Eltaso/Eltaso/CustomOperators.swift
+++ Eltaso/Eltaso/CustomOperators.swift
@@ -16,41 +16,44 @@ public func + <Key, Value> (lhs: Dictionary<Key, Value>, rhs: Dictionary<Key, Va
 	return dictionary
 }
 
+
 public func + (lhs: CGPoint, rhs: CGPoint) -> CGPoint {
 	return CGPoint(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
-}
-
-public func * (lhs: CGSize, rhs: CGFloat) -> CGSize {
-	return CGSize(width: lhs.width * rhs, height: lhs.height * rhs)
-}
-
-public func * (lhs: CGPoint, rhs: CGFloat) -> CGPoint {
-	return CGPoint(x: lhs.x * rhs, y: lhs.y * rhs)
-}
-
-public func *= (lhs: inout CGSize, rhs: CGFloat) {
-	lhs = lhs * rhs
-}
-
-public func *= (lhs: inout CGPoint, rhs: CGFloat) {
-	lhs = lhs * rhs
 }
 
 public func += (lhs: inout CGPoint, rhs: CGPoint) {
 	lhs = lhs + rhs
 }
 
+
+public func * (lhs: CGPoint, rhs: CGFloat) -> CGPoint {
+	return CGPoint(x: lhs.x * rhs, y: lhs.y * rhs)
+}
+
+public func * (lhs: CGSize, rhs: CGFloat) -> CGSize {
+	return CGSize(width: lhs.width * rhs, height: lhs.height * rhs)
+}
+
+public func *= (lhs: inout CGPoint, rhs: CGFloat) {
+	lhs = lhs * rhs
+}
+
+public func *= (lhs: inout CGSize, rhs: CGFloat) {
+	lhs = lhs * rhs
+}
+
+
 infix operator =? : AssignmentPrecedence
-
-infix operator …= : ComparisonPrecedence
-
-infix operator !…= : ComparisonPrecedence
 
 public func =? <T> (lhs: inout T, rhs: T?) {
 	if let rhs = rhs {
 		lhs = rhs
 	}
 }
+
+
+infix operator …= : ComparisonPrecedence
+infix operator !…= : ComparisonPrecedence
 
 public func …= <T> (lhs: Range<T>, rhs: Range<T>) -> Bool where T: Comparable {
 	if lhs.lowerBound >= rhs.lowerBound && lhs.upperBound <= rhs.upperBound {
@@ -76,6 +79,14 @@ public func …= <T> (lhs: CountableRange<T>, rhs: CountableRange<T>) -> Bool wh
 	}
 }
 
+public func …= <T> (lhs: CountableClosedRange<T>, rhs: CountableClosedRange<T>) -> Bool where T: Comparable {
+	if lhs.lowerBound >= rhs.lowerBound && lhs.upperBound <= rhs.upperBound {
+		return true
+	} else {
+		return false
+	}
+}
+
 public func …= <T> (lhs: T, rhs: Range<T>) -> Bool where T: Comparable {
 	if lhs >= rhs.lowerBound && lhs < rhs.upperBound {
 		return true
@@ -94,6 +105,14 @@ public func …= <T> (lhs: T, rhs: ClosedRange<T>) -> Bool where T: Comparable {
 
 public func …= <T> (lhs: T, rhs: CountableRange<T>) -> Bool where T: Comparable {
 	if lhs >= rhs.lowerBound && lhs < rhs.upperBound {
+		return true
+	} else {
+		return false
+	}
+}
+
+public func …= <T> (lhs: T, rhs: CountableClosedRange<T>) -> Bool where T: Comparable {
+	if lhs >= rhs.lowerBound && lhs <= rhs.upperBound {
 		return true
 	} else {
 		return false

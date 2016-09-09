95d532e395d4cffb47f41c07ceddd05fcbc5ee0e
史 翔新 <shi_xiangxin@mages.co.jp>
2016-09-09 21:44:10 +0900

    Int update

--- Eltaso/Eltaso/Int.swift
+++ Eltaso/Eltaso/Int.swift
@@ -8,42 +8,24 @@
 
 import Foundation
 
-public extension Int {
+extension Int {
 	
-	var increased: Int {
-		return self + 1
-	}
-	
-	var decreased: Int {
-		return self - 1
-	}
-	
-	func increased(by n: Int) -> Int {
-		return self + n
-	}
-	
-	func decreased(by n: Int) -> Int {
-		return self - n
-	}
-	
-	mutating func increase(by n: Int = 1) {
-		self = self.increased(by: n)
-	}
-	
-	mutating func decrease(by n: Int = 1) {
-		self = self.decreased(by: n)
-	}
-	
-	public static func createRandom(range: Range<Int>) -> Int {
+	public static func createRandom(range: CountableRange<Int>) -> Int {
+		
 		let rangeLength = range.upperBound - range.lowerBound
 		let random = arc4random_uniform(UInt32(rangeLength))
+		
 		return Int(random) + range.lowerBound
+		
 	}
 	
-	public static func createRandom(range: ClosedRange<Int>) -> Int {
+	public static func createRandom(range: CountableClosedRange<Int>) -> Int {
+		
 		let rangeLength = range.upperBound.increased - range.lowerBound
 		let random = arc4random_uniform(UInt32(rangeLength))
+		
 		return Int(random) + range.lowerBound
+		
 	}
 	
 }

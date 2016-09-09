7a24fd69e86d93ebb54d306310ed3c87935b2188
史 翔新 <shi_xiangxin@mages.co.jp>
2016-09-09 14:57:22 +0900

    Some new methods to String

--- Eltaso/Eltaso/String.swift
+++ Eltaso/Eltaso/String.swift
@@ -10,6 +10,48 @@ import Foundation
 
 public extension String {
 	
+	public mutating func keepFirst(_ n: Int = 1) {
+		self = self.keepingFirst(n)
+	}
+	
+	public mutating func keepLast(_ n: Int = 1) {
+		self = self.keepingLast(n)
+	}
+	
+}
+
+public extension String {
+	
+	public mutating func dropFirst(_ n: Int = 1) {
+		self = self.droppingFirst(n)
+	}
+	
+	public mutating func dropLast(_ n: Int = 1) {
+		self = self.droppingLast(n)
+	}
+	
+}
+
+public extension String {
+	
+	public func keepingFirst(_ n: Int = 1) -> String {
+		
+		let remainingIndex = self.characters.index(self.startIndex, offsetBy: n, limitedBy: self.endIndex) ?? self.endIndex
+		return self.substring(to: remainingIndex)
+		
+	}
+	
+	public func keepingLast(_ n: Int = 1) -> String {
+		
+		let remainingIndex = self.characters.index(self.endIndex, offsetBy: -n, limitedBy: self.startIndex) ?? self.startIndex
+		return self.substring(from: remainingIndex)
+		
+	}
+	
+}
+
+public extension String {
+
 	public func droppingFirst(_ n: Int = 1) -> String {
 		
 		let remainingIndex = self.characters.index(self.startIndex, offsetBy: n, limitedBy: self.endIndex) ?? self.endIndex

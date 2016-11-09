83faa1327c72d6c5874de17c092bc7d5487804f7
史 翔新 <shi_xiangxin@mages.co.jp>
2016-11-09 12:50:16 +0900

    CGPoint - and / operation

--- Eltaso/Eltaso/CGPoint.swift
+++ Eltaso/Eltaso/CGPoint.swift
@@ -16,6 +16,13 @@ public func += (lhs: inout CGPoint, rhs: CGPoint) {
 	lhs = lhs + rhs
 }
 
+public func - (lhs: CGPoint, rhs: CGPoint) -> CGPoint {
+	return CGPoint(x: lhs.x - rhs.x, y: lhs.y - rhs.y)
+}
+
+public func -= (lhs: inout CGPoint, rhs: CGPoint) {
+	lhs = lhs - rhs
+}
 
 public func * (lhs: CGPoint, rhs: CGFloat) -> CGPoint {
 	return CGPoint(x: lhs.x * rhs, y: lhs.y * rhs)
@@ -23,6 +30,14 @@ public func * (lhs: CGPoint, rhs: CGFloat) -> CGPoint {
 
 public func *= (lhs: inout CGPoint, rhs: CGFloat) {
 	lhs = lhs * rhs
+}
+
+public func / (lhs: CGPoint, rhs: CGFloat) -> CGPoint {
+	return CGPoint(x: lhs.x / rhs, y: lhs.y / rhs)
+}
+
+public func /= (lhs: inout CGPoint, rhs: CGFloat) {
+	lhs = lhs / rhs
 }
 
 extension CGPoint {

6e06eeb33edf30713bcb87489653d9cc95156318
史 翔新 <shi_xiangxin@mages.co.jp>
2016-11-10 19:50:47 +0900

    +-*/ operation for CGSize

--- Eltaso/Eltaso/CGSize.swift
+++ Eltaso/Eltaso/CGSize.swift
@@ -8,6 +8,39 @@
 
 import Foundation
 
+public func + (lhs: CGSize, rhs: CGSize) -> CGSize {
+	return CGSize(width: lhs.width + rhs.width, height: lhs.height + rhs.height)
+}
+
+public func += (lhs: inout CGSize, rhs: CGSize) {
+	lhs = lhs + rhs
+}
+
+public func - (lhs: CGSize, rhs: CGSize) -> CGSize {
+	return CGSize(width: lhs.width - rhs.width, height: lhs.height - rhs.height)
+}
+
+public func -= (lhs: inout CGSize, rhs: CGSize) {
+	lhs = lhs - rhs
+}
+
+public func * (lhs: CGSize, rhs: CGFloat) -> CGSize {
+	return CGSize(width: lhs.width * rhs, height: lhs.height * rhs)
+}
+
+public func *= (lhs: inout CGSize, rhs: CGFloat) {
+	lhs = lhs * rhs
+}
+
+public func / (lhs: CGSize, rhs: CGFloat) -> CGSize {
+	return CGSize(width: lhs.width / rhs, height: lhs.height / rhs)
+}
+
+public func /= (lhs: inout CGSize, rhs: CGFloat) {
+	lhs = lhs / rhs
+}
+
+
 extension CGSize {
 	
 	public init(length: CGFloat) {
@@ -112,12 +145,4 @@ extension CGSize {
 		return CGSize(width: self.width - (horizontalMargin * 2), height: self.height - (verticalMargin * 2))
 	}
 	
-}
-
-public func * (lhs: CGSize, rhs: CGFloat) -> CGSize {
-	return CGSize(width: lhs.width * rhs, height: lhs.height * rhs)
-}
-
-public func *= (lhs: inout CGSize, rhs: CGFloat) {
-	lhs = lhs * rhs
 }

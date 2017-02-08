904ecb375af2b9671ede3e1bad4e324c36562ff9
史 翔新 <shi_xiangxin@mages.co.jp>
2017-02-08 12:02:41 +0900

    TranslatableView methods minor update due to CGPoint and CGVector's duty's update

--- Eltaso/Eltaso/TranslatableView.swift
+++ Eltaso/Eltaso/TranslatableView.swift
@@ -9,13 +9,19 @@
 import UIKit
 
 public protocol TranslatableView: class {
+	@available(*, deprecated: 3.1.1, message: "Use translate(by: CGVector) instead")
 	func translate(by translation: CGPoint)
+	func translate(by translation: CGVector)
 }
 
 extension TranslatableView where Self: UIView {
 	
 	public func translate(by translation: CGPoint) {
 		self.transform.translateBy(x: translation.x, y: translation.y)
+	}
+	
+	public func translate(by translation: CGVector) {
+		self.transform.translateBy(x: translation.dx, y: translation.dy)
 	}
 	
 }

1fd39d3c70c6b00625f9ce9807695b4a5704efbc
史 翔新 <shi_xiangxin@mages.co.jp>
2017-02-24 14:22:11 +0900

    Add functions for CGSize * CGScale and CGSize / CGScale

--- Eltaso/Eltaso/CGSize.swift
+++ Eltaso/Eltaso/CGSize.swift
@@ -32,11 +32,27 @@ public func *= (lhs: inout CGSize, rhs: CGFloat) {
 	lhs = lhs * rhs
 }
 
+public func * (lhs: CGSize, rhs: CGScale) -> CGSize {
+	return CGSize(width: lhs.width * rhs.horizontal, height: lhs.height * rhs.vertical)
+}
+
+public func *= (lhs: inout CGSize, rhs: CGScale) {
+	lhs = lhs * rhs
+}
+
 public func / (lhs: CGSize, rhs: CGFloat) -> CGSize {
 	return CGSize(width: lhs.width / rhs, height: lhs.height / rhs)
 }
 
 public func /= (lhs: inout CGSize, rhs: CGFloat) {
+	lhs = lhs / rhs
+}
+
+public func / (lhs: CGSize, rhs: CGScale) -> CGSize {
+	return CGSize(width: lhs.width / rhs.horizontal, height: lhs.height / rhs.vertical)
+}
+
+public func /= (lhs: inout CGSize, rhs: CGScale) {
 	lhs = lhs / rhs
 }
 
